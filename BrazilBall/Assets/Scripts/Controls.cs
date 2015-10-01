1b4701c174a218c039de242e958d395a6a53bb01
Refain <samuelcalebe@yahoo.com.br>
2015-10-01 14:52:13 -0300

    Revert "Animação Pulo"
    This reverts commit c3822865ef0ed729aa8ee511da4496647594f3f4.

--- BrazilBall/Assets/Scripts/Controls.cs
+++ BrazilBall/Assets/Scripts/Controls.cs
@@ -7,6 +7,5 @@ public static class Controls : object
  
     public static KeyCode walkLeft = KeyCode.A;
     public static KeyCode walkRight = KeyCode.D;
-    public static KeyCode jump = KeyCode.Space;
-
+  
 }
--- BrazilBall/Assets/Scripts/PlayerController.cs
+++ BrazilBall/Assets/Scripts/PlayerController.cs
@@ -6,25 +6,20 @@ public class PlayerController : MonoBehaviour{
     public float velocidade = 1f;
     private bool right = true;
     public Animator animator;
-    public bool isgrounded;
-    public Transform ground;
-    public int force;
-  
+    
+
     // Executado uma vez e logo no inicio.
     void Start(){
-      }
+       
+    }
     // Chamado a cada frame do jogo.(Uma grande quantidade de linhas no update pode gerar em alguns jogos um frame ruim)
-    void Update()
-    {
-        isgrounded = Physics2D.Linecast(this.transform.position,ground.position , 1 <<  LayerMask.NameToLayer("ground"));
-
-        if (Input.GetKey(Controls.walkLeft))
-        { //Se o A(Ele chama o script Controls que é estatico) for apertado ele vai ativar a animação de correr.
+    void Update(){
+        
+        if (Input.GetKey(Controls.walkLeft) ) { //Se o A(Ele chama o script Controls que é estatico) for apertado ele vai ativar a animação de correr.
             animator.SetBool("run", true);
             transform.Translate(new Vector3(-velocidade * Time.deltaTime, 0, 0));
-
-            if (right)
-            {
+            
+            if (right){
                 Flip();
             }
         }
@@ -32,28 +27,13 @@ public class PlayerController : MonoBehaviour{
         {
             animator.SetBool("run", true);
             transform.Translate(new Vector3(velocidade * Time.deltaTime, 0, 0));
-            if (!right)
-            {
+            if (!right){
                 Flip();
             }
         }
         else
         {
             animator.SetBool("run", false);
-            
-        }
-
-
-        if (Input.GetKey(Controls.jump)    && isgrounded)
-        {
-            animator.SetBool("jump", true);
-            myRigidBody2D.AddForce(transform.up * force);
-                
-        }
-         else
-        {
-            
-            animator.SetBool("jump", false);
         }
     }
     private void Flip(){
