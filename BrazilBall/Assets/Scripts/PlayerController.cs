fe42b68c64e15d3991a9f3cf151a3c087f7b5ae2
Refain <samuelcalebe@yahoo.com.br>
2015-10-01 20:29:10 -0300

    Tirei os negocios do pulo,tem que refazer

--- BrazilBall/Assets/Scripts/PlayerController.cs
+++ BrazilBall/Assets/Scripts/PlayerController.cs
@@ -6,9 +6,7 @@ public class PlayerController : MonoBehaviour{
     public float velocidade = 1f;
     private bool right = true;
     public Animator animator;
-    public bool isgrounded;
-    public Transform ground;
-    public int force;
+
 
     // Executado uma vez e logo no inicio.
     void Start(){
@@ -16,7 +14,7 @@ public class PlayerController : MonoBehaviour{
     }
     // Chamado a cada frame do jogo.(Uma grande quantidade de linhas no update pode gerar em alguns jogos um frame ruim)
     void Update(){
-        isgrounded = Physics2D.Linecast(this.transform.position, ground.position, 1 << LayerMask.NameToLayer("ground"));
+        
 
         if (Input.GetKey(Controls.walkLeft) )
         { //Se o A(Ele chama o script Controls que é estatico) for apertado ele vai ativar a animação de correr.
@@ -39,17 +37,7 @@ public class PlayerController : MonoBehaviour{
         {
             animator.SetBool("run", false);
         }
-        if (Input.GetKey(Controls.jump) && isgrounded)
-        {
-            animator.SetBool("jump", true);
-            myRigidBody2D.AddForce(transform.up * force);
-
-        }
-        else
-        {
-
-            animator.SetBool("jump", false);
-        }
+        
     }
     private void Flip(){
         // Troca pra onde o player ta olhando.
