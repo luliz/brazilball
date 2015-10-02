c27ed3520d5df51f8773617c518440d6ea6ebe19
Lulinha <luiz.viana000@gmail.com>
2015-10-02 12:47:43 -0300

    Adicionado pulo

--- BrazilBall/Assets/Scripts/CameraFollowing.cs
+++ BrazilBall/Assets/Scripts/CameraFollowing.cs
@@ -13,7 +13,7 @@ public class CameraFollowing : MonoBehaviour {
 		Vector3 point = GetComponent<Camera>().WorldToViewportPoint(target.position);
 		Vector3 delta = target.position - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
 		Vector3 destination = transform.position + delta;
-		if (!(transform.position.y - destination.y > 0.5 || destination.y - transform.position.y > 0.5))
+		if (!(transform.position.y - destination.y > 1 || destination.y - transform.position.y > 1))
 			destination.y = transform.position.y;
 
 		transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
--- BrazilBall/Assets/Scripts/PlayerController.cs
+++ BrazilBall/Assets/Scripts/PlayerController.cs
@@ -2,19 +2,20 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour{
+
     public Rigidbody2D myRigidBody2D;
     public float velocidade = 1f;
     private bool right = true;
     public Animator animator;
+	public float forcaPulo = 10;
+	public bool grounded;
 
 
     // Executado uma vez e logo no inicio.
     void Start(){
-       
     }
     // Chamado a cada frame do jogo.(Uma grande quantidade de linhas no update pode gerar em alguns jogos um frame ruim)
     void Update(){
-        
 
         if (Input.GetKey(Controls.walkLeft) )
         { //Se o A(Ele chama o script Controls que é estatico) for apertado ele vai ativar a animação de correr.
@@ -37,6 +38,11 @@ public class PlayerController : MonoBehaviour{
         {
             animator.SetBool("run", false);
         }
+
+		if (Input.GetKeyDown (Controls.jump) && grounded) {
+
+			myRigidBody2D.AddForce(new Vector2 (0, forcaPulo), ForceMode2D.Impulse);
+		}
         
     }
     private void Flip(){
