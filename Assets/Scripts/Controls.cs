bd7234d93c31537443899e5521d17d572d3d2907
Lulinha <luiz.viana000@gmail.com>
2015-10-03 17:31:04 -0300

    Adicionado assets e background

---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Controls : object
+{
+
+ 
+    public static KeyCode walkLeft = KeyCode.A;
+    public static KeyCode walkRight = KeyCode.D;
+    public static KeyCode jump = KeyCode.Space;
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour{
+
+    public Rigidbody2D myRigidBody2D;
+    public float velocidade = 1f;
+    private bool right = true;
+    public Animator animator;
+	public float forcaPulo = 10;
+	public bool grounded;
+
+
+    // Executado uma vez e logo no inicio.
+    void Start(){
+    }
+    // Chamado a cada frame do jogo.(Uma grande quantidade de linhas no update pode gerar em alguns jogos um frame ruim)
+    void Update(){
+
+        if (Input.GetKey(Controls.walkLeft) )
+        { //Se o A(Ele chama o script Controls que é estatico) for apertado ele vai ativar a animação de correr.
+            animator.SetBool("run", true);
+            transform.Translate(new Vector3(-velocidade * Time.deltaTime, 0, 0));
+            
+            if (right){
+                Flip();
+            }
+        }
+        else if (Input.GetKey(Controls.walkRight))
+        {
+            animator.SetBool("run", true);
+            transform.Translate(new Vector3(velocidade * Time.deltaTime, 0, 0));
+            if (!right){
+                Flip();
+            }
+        }
+        else
+        {
+            animator.SetBool("run", false);
+        }
+
+		if (Input.GetKeyDown (Controls.jump) && grounded) {
+
+			myRigidBody2D.AddForce(new Vector2 (0, forcaPulo), ForceMode2D.Impulse);
+		}
+        
+    }
+    private void Flip(){
+        // Troca pra onde o player ta olhando.
+        right = !right;
+        // Vira o player .
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
+
+
+
+
+
