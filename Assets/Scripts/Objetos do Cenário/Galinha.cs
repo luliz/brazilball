5704dad61376e57d6a2dc01b0336c8a516ad6a68
Lulinha <luiz.viana000@gmail.com>
2015-10-06 17:05:21 -0300

    Update da galinha

---
+++ Assets/Scripts/Objetos do Cenário/Galinha.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Galinha : MonoBehaviour {
+
+
+	private float timeToBegin;
+
+	void Start() {
+
+		timeToBegin = Random.value * 2;
+		
+	}
+	// Update is called once per frame
+	void Update () {
+
+		if (timeToBegin <= 0) {
+
+			this.GetComponent<Animator>().SetTrigger("mover");
+			timeToBegin = Random.value * 4;
+		}
+		timeToBegin -= Time.deltaTime;
+	}
+}
