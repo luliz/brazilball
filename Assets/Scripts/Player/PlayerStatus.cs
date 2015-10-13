ae394ee38b2fd4c25f7c5d8aeb6e54da595fbbe8
Lulinha <luiz.viana000@gmail.com>
2015-10-13 02:47:48 -0300

    Pisca quando leva hit

--- Assets/Scripts/Player/PlayerStatus.cs
+++ Assets/Scripts/Player/PlayerStatus.cs
@@ -7,12 +7,41 @@ public class PlayerStatus : MonoBehaviour {
 	public float maxImmuneTime = 2f;
 	public float counter;
 
+	private bool blink;
+	
+	public Color blinkColor;
+	private SpriteRenderer spriteRenderer;
+	private float timeBetweenBlinks = 0.25f;
+	private float blinkCounter = 0;
+
+	void Awake () {
+
+		spriteRenderer = this.GetComponent<SpriteRenderer> ();
+	}
 	void Update () {
 		counter += Time.deltaTime;
+		if (blink) {
+			print (Mathf.Round (counter * 100f) / 100f % timeBetweenBlinks);
+			if (Mathf.Round (counter * 100f) / 100f % timeBetweenBlinks < 0.1) {
+
+				spriteRenderer.color = blinkColor;
+			}
+			else {
+
+				spriteRenderer.color = Color.white;
+			}
+			if (counter > maxImmuneTime) {
+
+				spriteRenderer.color = Color.white;
+				blink = false;
+			}
+			
+		}
 	}
 	public void TakeDamage (int direction) {
 
 		if (counter > maxImmuneTime) {
+			blink = true;
 			counter = 0;
 			this.GetComponent<Rigidbody2D>().AddForce(new Vector2 (direction * 10, 0), ForceMode2D.Impulse);
 			if (lives == 1) {
