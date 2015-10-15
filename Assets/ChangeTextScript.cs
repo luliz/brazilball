51dc5f04a20e5312a5424ef1daf893a277515af2
Lulinha <luiz.viana000@gmail.com>
2015-10-15 09:20:18 -0300

    Implementado sistema de mudar a tecla de cada ação

---
+++ Assets/ChangeTextScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ChangeTextScript : MonoBehaviour {
+
+	public string type;
+
+	Text text;
+	void Awake () {
+
+		text = this.GetComponent<Text> ();
+	}
+	// Update is called once per frame
+	void Update () {
+
+		UpdateText ();
+	}
+
+	public void UpdateText () {
+
+		text.text = type;
+
+		switch (type) {
+		case "Jump: ":
+			text.text += Controls.jump.ToString();
+			break;
+		case "Attack: ":
+			text.text += Controls.strongAttack.ToString();
+			break;
+		case "Pause: ":
+			text.text += Controls.pause.ToString();
+			break;
+		case "Walk Left: ":
+			text.text += Controls.walkLeft.ToString();
+			break;
+		case "Walk Right: ":
+			text.text += Controls.walkRight.ToString();
+			break;
+		}
+	}
+}
--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -8,15 +8,67 @@ public class GameManager : MonoBehaviour {
 
 	public GameObject mainMenu;
 	public GameObject areYouSure;
+	public GameObject changeKeyGUI;
+	public GameObject pressKeyNowGUI;
+
+	public static GameObject pressKeyNowCreated = null;
 	public static GameObject menuCreated = null;
 	public static GameObject windowCreated = null;
 
-	void Awake () {
+	private static bool allowed = true;
+	static bool changeKey;
+	static string key;
 
+	int next = 0;
+	void Awake () {
 		status = GameObject.Find ("Player").GetComponent<PlayerStatus> ();
 	}
+
+	void OnGUI () {
+
+		if (changeKey) {
+
+			Event e = Event.current;
+			if (next > 0) {
+				next++;
+				if (next > 10) {
+					changeKey = false;
+					next = 0;
+					Button[] buttons = windowCreated.GetComponentsInChildren<Button> ();
+					for (int i = 0; i < buttons.Length; i++) {
+					
+						buttons[i].interactable = true;
+					}
+				}
+			}
+			else if (e.type.Equals (EventType.KeyDown)) {
+
+				KeyCode k = e.keyCode;
+				switch (key) {
+				case "jump":
+					Controls.jump = k;
+					break;
+				case "attack":
+					Controls.strongAttack = k;
+					break;
+				case "pause":
+					Controls.pause = k;
+					break;
+				case "walkLeft":
+					Controls.walkLeft = k;
+					break;
+				case "walkRight":
+					Controls.walkRight = k;
+					break;
+				}
+				Destroy (pressKeyNowCreated);
+				next = 1;
+			}
+		}
+	}
 	void Update () {
-		if (Input.GetKeyDown (Controls.pause) && !status.gameOver) {
+		print (allowed);
+		if (Input.GetKeyDown (Controls.pause) && !status.gameOver && allowed) {
 			if (menuCreated) {
 				DestroyMenu ();
 			}
@@ -64,5 +116,30 @@ public class GameManager : MonoBehaviour {
 			buttons[i].interactable = true;
 		}
 		Destroy (windowCreated);
+	}
+
+	public void ChangeKey (string keyToChange) {
+		Button[] buttons = windowCreated.GetComponentsInChildren<Button> ();
+		for (int i = 0; i < buttons.Length; i++) {
+			
+			buttons[i].interactable = false;
+		}
+		pressKeyNowCreated = Instantiate (pressKeyNowGUI);
+		key = keyToChange;
+		changeKey = true;
+
+	}
+
+	public void OpenChangeKeysMenu () {
+		allowed = false;
+		print ("shit");
+		Destroy (menuCreated);
+		windowCreated = Instantiate (changeKeyGUI);
+	}
+
+	public void ReturnToMenu () {
+		allowed = true;
+		Destroy (windowCreated);
+		menuCreated = Instantiate (mainMenu);
 	}
 }
