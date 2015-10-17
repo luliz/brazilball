777f5b505a8f942d2c558085a23bc14cf1f65718
Refain <samuelcalebe@yahoo.com.br>
2015-10-16 22:20:35 -0300

    Flechas do céu(50%)
    Ainda falta ajeitar um bucado de coisa e eu fiz de uma maneira bem
    idiota,mas ta funcionando,vou ver se melhoro mais tarde

--- Assets/Scripts/Enemies/Flechas.cs
+++ Assets/Scripts/Enemies/Flechas.cs
@@ -1,16 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class arroww : MonoBehaviour
-{
+public class Flechas : MonoBehaviour {
+
 
     private Transform target;
+
     private float absDistance;
-    private float Power;  
+    private float Power;
     private Rigidbody2D myRB;
     private Collider2D myCL;
-
-
+  
     void Start()
     {
         myRB = GetComponent<Rigidbody2D>();
@@ -18,71 +18,63 @@ public class arroww : MonoBehaviour
         target = GameObject.FindWithTag("Player").transform;
 
         absDistance = Mathf.Abs(transform.position.x - target.position.x);
-        Power = (1000 * absDistance) / 13;
+        Power = (1000 * absDistance) / 12;
 
         if (target.position.x > transform.position.x)
         {
-            if (Mathf.Abs(target.position.x - transform.position.x) < 4)
-            {
-                myRB.AddForce(new Vector2(600, 50));
-            }
-            else
-            {
-                myRB.AddForce(new Vector2(Power, 155));
-            }
-
+                          
+          
+                myRB.AddForce(new Vector2(Power, 100));
+            
 
         }
 
         else if (target.position.x < transform.position.x)
         {
-
-            if (Mathf.Abs(target.position.x - transform.position.x) < 4)
-            {
-                myRB.AddForce(new Vector2(-600, 80));
-            }
-            else
-            {
-                myRB.AddForce(new Vector2(-Power, 155));
-            }
+                    
+            
+            
+                myRB.AddForce(new Vector2(-Power, 100));
+            
         }
         Destroy(gameObject, 3);
-        
+
     }
-    
+
     void OnCollisionEnter2D(Collision2D other)
     {
-       
+
         if (other.gameObject.tag == "Player")
         {
-            
+
             transform.parent = other.transform;
             Destroy(myRB);
             Destroy(myCL);
-			other.gameObject.GetComponent<PlayerStatus>().TakeDamage(1);
-          
+            other.gameObject.GetComponent<PlayerStatus>().TakeDamage(1);
+
 
 
         }
-        else if ((other.gameObject.tag != "enemyArcher" ))
+        else if ((other.gameObject.tag != "enemyArcher") && other.gameObject.tag != "arrow")
         {
             Destroy(myRB);
             Destroy(myCL);
         }
     }
+
+   
+           
+           
+    
     void Update()
     {
         if (myRB != null)
         {
             Vector3 dir = myRB.velocity;
             float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);            
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         }
-        
-    }
 
-
-
-
-
+    
+}
 }
---
+++ Assets/Scripts/PontoF.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PontoF : MonoBehaviour {
+    public Transform arrow3;
+    public Transform PontaF;
+    public float contador;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (contador < Time.time)
+        {
+            Instantiate(arrow3, PontaF.transform.position, Quaternion.identity);
+            contador = Time.time + 3f;
+        }
+    }
+}
