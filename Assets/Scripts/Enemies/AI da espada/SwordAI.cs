f8bdbd383f4bdd0a223db630ac7cddae00318480
Refain <samuelcalebe@yahoo.com.br>
2015-10-28 21:32:48 -0200

    Refazendo algumas coisas
    Não ta terminado,relaxem

--- Assets/Scripts/Enemies/AI da espada/SwordAI.cs
+++ Assets/Scripts/Enemies/AI da espada/SwordAI.cs
@@ -1,111 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SwordAI : MonoBehaviour {
+public class SwordAI : BasicAI
+{
 
-    private int estado = 0;
+    public int health = 5;
 
-    //	VARIAVEIS QUE FAZEM REFERENCIA A COMPONENTES
-
-    private Animator thisAnimator;
-    private Animator SwordAnimator;
-    private Rigidbody2D thisCollider;
-    private SpriteRenderer expression;
-    public Sprite exclamation;
-    public Sprite interrogation;
-
-    //  INICIALIZACAO DAS DEMAIS VARIAVEIS
-
-    private RaycastHit2D saw; //Variavel que guarda informacoes sobre o raycast da visao do janissary
-    private Transform target; //transform da poland
-    private float timeAfterIsaw;//Tempo que faz desde que a polonia foi avistada. (serve pra decidir quando o janissary deve desistir de procurar)
-    private int facingDirection = 1;//1=direita, -1=esquerda
-    public LayerMask visaoJanissary;//Layer mask q armazena quais objetos podem ser 'vistos' pelo janissary. (serve principalmente para impedir q sua visao pare em si mesmo (em seu proprio collider)
-    public float speed;
-    public LayerMask barreiras;
-    private int decision;
+    private float memory = 3.5f;
+    private float minDistanceToAttack = 0f;
+    private float maxDistanceToAttack = 2f;
+    private float maxDistanceToFollow = 40f;
+    private float maxVerticalDistanceToAttack = 0.5f;
+    private float attackCounter;
+    private float timeToAttack = 0.3f;
+    public float chargePower = 10;
+    public BoxCollider2D spearCollider;
     private float counter;
-    private float spriteCounter;
-    private float contador3;
+    private bool attacked = false;
 
+    private Animator SwordAnimator;
+
+    // Update is called once per frame
     void Awake()
     {
-        target = GameObject.FindGameObjectWithTag("Player").transform;
-        thisAnimator = GetComponent<Animator>();
-        SwordAnimator = GameObject.Find("EspadaAI").GetComponent<Animator>();
-        thisCollider = GetComponent<Rigidbody2D>();
-        expression = GameObject.Find("Expressions3").GetComponent<SpriteRenderer>();
+          SwordAnimator = GameObject.Find("EspadaAI").GetComponent<Animator>();
     }
-    
-    void Update()
-
-    {
-        
-        if (Found())
-            estado = 1;
-
-        if (estado == 0)
-            Search();
-        else if (estado == 1)
-            Follow();
-        else
-            Attack();
-            
-
-        timeAfterIsaw += Time.deltaTime;
-        Flip();
-
-        if (expression.sprite != null)
-        {
-            spriteCounter += Time.deltaTime;
-            if (spriteCounter > 2)
-            {
-                spriteCounter = 0;
-                expression.sprite = null;
-            }
-        }
-    }
-
-    void Search()
+    public override void Follow()
     {
 
-        if (decision == 1)
-        {
-            facingDirection = -1;
-            Walk();
-        }
-        else if (decision == 2)
-        {
-            facingDirection = 1;
-            Walk();
-        }
-        else
-        {
-            thisAnimator.SetBool("walking", false);
-        }
-
-        counter += Time.deltaTime;
-
-        if (counter > 0.5)
-        {
-            decision = (int)Random.Range(1, 4);
-            counter = 0;
-        }
-    }
-
-    void Follow()
-    {
-        if (timeAfterIsaw > 3.5)
+        if (timeAfterISaw > memory)
         {
             estado = 0;
-            expression.sprite = interrogation;
+            if (expressions)
+                expressions.sprite = interrogation;
         }
 
-        if (Mathf.Abs(target.position.x - transform.position.x) >= 0.5f && Mathf.Abs(target.position.x - transform.position.x) < 10f)
+        if (Mathf.Abs(target.position.x - transform.position.x) > maxDistanceToAttack && Mathf.Abs(target.position.x - transform.position.x) < maxDistanceToFollow)
         {
             Walk();
-            thisAnimator.SetBool("walking", true);
-            SwordAnimator.SetBool("atacar", false);
 
             if (transform.position.x > target.position.x)
             {
@@ -115,89 +47,95 @@ public class SwordAI : MonoBehaviour {
             {
                 facingDirection = 1;
             }
-
         }
-        else  
+        else if (Mathf.Abs(target.position.x - transform.position.x) < maxDistanceToAttack && Mathf.Abs(target.position.x - transform.position.x) > minDistanceToAttack)
         {
-            thisAnimator.SetBool("walking", false);
+
+            animator.SetBool("walk", false);
             SwordAnimator.SetBool("atacar", true);
-
-
-
             if (transform.position.x > target.position.x)
             {
                 facingDirection = -1;
 
-
             }
             else
             {
+
                 facingDirection = 1;
 
             }
 
+            if (Mathf.Abs(target.position.y - transform.position.y) < maxVerticalDistanceToAttack)
+            {
 
+                if (counter >= 1.5)
+                {
+                    animator.SetInteger("status", 1);
+                    attackCounter = 0;
+                    estado = 2;
+                    canFlip = false;
+                }
+
+            }
         }
-    }      
-
-    void Attack()
-    {       
-        
-        //TODO depois da animacao feita fazer o script de ataque
-        //por enquanto ele simplesmente retorna ao estado 1
-        
     }
 
-    bool Found()
+    public override void Attack()
     {
-        saw = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - 0.1f), Vector2.right * facingDirection, 5f, visaoJanissary);
-        if (saw.transform == target)
+
+        attackCounter += Time.deltaTime;
+        if (attackCounter >= timeToAttack)
         {
-            
-            timeAfterIsaw = 0;
-            if (estado == 0)
-                expression.sprite = exclamation;
-            
-            return true;
+            if (!attacked)
+            {
+                this.GetComponent<Rigidbody2D>().AddForce(Vector2.right * transform.localScale.x * chargePower, ForceMode2D.Impulse);
+                SwordAnimator.SetBool("atacar", true);
+                spearCollider.enabled = true;
+                attacked = true;
+            }
+            else
+            {
+
+                if (Mathf.Abs(this.GetComponent<Rigidbody2D>().velocity.x) <= 4)
+                {
+
+                    spearCollider.enabled = false;
+                    canFlip = true;
+                    attacked = false;
+                    animator.SetInteger("status", 0);
+                    estado = 1;
+                    counter = 0;
+                }
+            }
         }
-        return false;
     }
 
-    void Walk()
+    public override void DoExtraStuff()
+    {
+        counter += Time.deltaTime;
+    }
+
+    public override void TakeDamage()
     {
 
-        if (thisCollider.IsTouchingLayers(barreiras))
-            thisAnimator.SetBool("walking", false);
+        if (health > 0)
+        {
+            health--;
+        }
         else
         {
-            thisAnimator.SetBool("walking", true);
-            transform.Translate(new Vector2(speed * facingDirection * Time.deltaTime, 0));
+            GameManager.enemiesKilled.Add(this.enemyID);
+            Destroy(this.gameObject);
         }
-    }
-
-
-    //Mudei um pouco o funcionamento do Flip()
-    //Agora ele eh chamado todo frame
-    //fazendo o flip quando necessario
-    //diminuindo a chance de bugs
-    //quem conseguir pensar numa maneira menos idiota de fazer isso pode faze-lo
-    //talvez eu mesmo faça isso dps
-
-    void Flip()
-    {
-        transform.localScale = new Vector3(facingDirection, 1, 1);
-        expression.transform.localScale = new Vector3(facingDirection, 1, 1);
-
-    }
-
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject.tag=="limites")
-        {
-            
-            thisAnimator.SetBool("walking", false);
-
-        }
-        
     }
 }
+
+
+
+
+
+
+
+
+
+    
--- Assets/Scripts/Enemies/AI da espada/SwordB.cs
+++ Assets/Scripts/Enemies/AI da espada/SwordB.cs
@@ -1,15 +1,199 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿//using UnityEngine;
+//using System.Collections;
 
-public class SwordB : MonoBehaviour {
+//public class SwordB : MonoBehaviour
+//{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-}
+//    private int estado = 0;
+
+//    //	VARIAVEIS QUE FAZEM REFERENCIA A COMPONENTES
+
+//    private Animator thisAnimator;
+//    private Animator SwordAnimator;
+//    private Rigidbody2D thisCollider;
+//    private SpriteRenderer expression;
+//    public Sprite exclamation;
+//    public Sprite interrogation;
+
+//    //  INICIALIZACAO DAS DEMAIS VARIAVEIS
+
+//    private RaycastHit2D saw; //Variavel que guarda informacoes sobre o raycast da visao do janissary
+//    private Transform target; //transform da poland
+//    private float timeAfterIsaw;//Tempo que faz desde que a polonia foi avistada. (serve pra decidir quando o janissary deve desistir de procurar)
+//    private int facingDirection = 1;//1=direita, -1=esquerda
+//    public LayerMask visaoJanissary;//Layer mask q armazena quais objetos podem ser 'vistos' pelo janissary. (serve principalmente para impedir q sua visao pare em si mesmo (em seu proprio collider)
+//    public float speed;
+//    public LayerMask barreiras;
+//    private int decision;
+//    private float counter;
+//    private float spriteCounter;
+//    private float contador3;
+
+//    void Awake()
+//    {
+//        target = GameObject.FindGameObjectWithTag("Player").transform;
+//        thisAnimator = GetComponent<Animator>();
+//        SwordAnimator = GameObject.Find("EspadaAI").GetComponent<Animator>();
+//        thisCollider = GetComponent<Rigidbody2D>();
+//        expression = GameObject.Find("Expressions3").GetComponent<SpriteRenderer>();
+//    }
+
+//    void Update()
+
+//    {
+
+//        if (Found())
+//            estado = 1;
+
+//        if (estado == 0)
+//            Search();
+//        else if (estado == 1)
+//            Follow();
+//        else
+//            Attack();
+
+
+//        timeAfterIsaw += Time.deltaTime;
+//        Flip();
+
+//        if (expression.sprite != null)
+//        {
+//            spriteCounter += Time.deltaTime;
+//            if (spriteCounter > 2)
+//            {
+//                spriteCounter = 0;
+//                expression.sprite = null;
+//            }
+//        }
+//    }
+
+//    void Search()
+//    {
+
+//        if (decision == 1)
+//        {
+//            facingDirection = -1;
+//            Walk();
+//        }
+//        else if (decision == 2)
+//        {
+//            facingDirection = 1;
+//            Walk();
+//        }
+//        else
+//        {
+//            thisAnimator.SetBool("walking", false);
+//        }
+
+//        counter += Time.deltaTime;
+
+//        if (counter > 0.5)
+//        {
+//            decision = (int)Random.Range(1, 4);
+//            counter = 0;
+//        }
+//    }
+
+//    void Follow()
+//    {
+//        if (timeAfterIsaw > 3.5)
+//        {
+//            estado = 0;
+//            expression.sprite = interrogation;
+//        }
+
+//        if (Mathf.Abs(target.position.x - transform.position.x) >= 0.5f && Mathf.Abs(target.position.x - transform.position.x) < 10f)
+//        {
+//            Walk();
+//            thisAnimator.SetBool("walking", true);
+//            SwordAnimator.SetBool("atacar", false);
+
+//            if (transform.position.x > target.position.x)
+//            {
+//                facingDirection = -1;
+//            }
+//            else
+//            {
+//                facingDirection = 1;
+//            }
+
+//        }
+//        else
+//        {
+//            thisAnimator.SetBool("walking", false);
+//            SwordAnimator.SetBool("atacar", true);
+
+
+
+//            if (transform.position.x > target.position.x)
+//            {
+//                facingDirection = -1;
+
+
+//            }
+//            else
+//            {
+//                facingDirection = 1;
+
+//            }
+
+
+//        }
+//    }
+
+
+
+//    bool Found()
+//    {
+//        saw = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - 0.1f), Vector2.right * facingDirection, 5f, visaoJanissary);
+//        if (saw.transform == target)
+//        {
+
+//            timeAfterIsaw = 0;
+//            if (estado == 0)
+//                expression.sprite = exclamation;
+
+//            return true;
+//        }
+//        return false;
+//    }
+
+//    void Walk()
+//    {
+
+//        if (thisCollider.IsTouchingLayers(barreiras))
+//            thisAnimator.SetBool("walking", false);
+//        else
+//        {
+//            thisAnimator.SetBool("walking", true);
+//            transform.Translate(new Vector2(speed * facingDirection * Time.deltaTime, 0));
+//        }
+//    }
+
+
+//    //Mudei um pouco o funcionamento do Flip()
+//    //Agora ele eh chamado todo frame
+//    //fazendo o flip quando necessario
+//    //diminuindo a chance de bugs
+//    //quem conseguir pensar numa maneira menos idiota de fazer isso pode faze-lo
+//    //talvez eu mesmo faça isso dps
+
+//    void Flip()
+//    {
+//        transform.localScale = new Vector3(facingDirection, 1, 1);
+//        expression.transform.localScale = new Vector3(facingDirection, 1, 1);
+
+//    }
+
+//    void OnTriggerEnter2D(Collider2D other)
+//    {
+//        if (other.gameObject.tag == "limites")
+//        {
+
+//            thisAnimator.SetBool("walking", false);
+
+//        }
+
+//    }
+//}
+
