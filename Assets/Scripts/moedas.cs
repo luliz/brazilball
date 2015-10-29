bbf999e44122ecfbc419c3e81159561f28e07fd1
Refain <samuelcalebe@yahoo.com.br>
2015-10-29 20:19:19 -0200

    Arrumando algumas coisas
    Moedas
    Ceuta

---
+++ Assets/Scripts/moedas.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moedas : MonoBehaviour {
+
+    public Animator animator;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        animator.SetBool("movimento",true);
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {           
+            Destroy(this.gameObject);            
+        }
+
+
+    }
+}
