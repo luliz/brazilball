ff32e17c73233e480f0f28edb2714dbc6c6e60ed
Lulinha <luiz.viana000@gmail.com>
2015-10-30 20:16:54 -0300

    Add barra de hp do burrinho

--- Assets/Scripts/Burrinho.cs
+++ Assets/Scripts/Burrinho.cs
@@ -1,12 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Burrinho : MonoBehaviour {
 
 	public float speed = 5f;
+	public float health;
+
+	public Image filledHPImage;
 
 	void Update () {
 
 		transform.Translate (Vector3.right * speed * Time.deltaTime);
+
+		filledHPImage.rectTransform.sizeDelta = new Vector2 (health, filledHPImage.rectTransform.rect.size.y);
+		filledHPImage.rectTransform.anchoredPosition = new Vector2 (-505f - (100f - health) / 2f, filledHPImage.rectTransform.anchoredPosition.y);
+		if (Input.GetKeyDown (KeyCode.Return)) {
+
+			health -= 10f;
+		}
+		else if (health == 0){
+
+			Destroy (gameObject);
+		}
 	}
 }
