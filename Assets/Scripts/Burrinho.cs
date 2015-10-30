38ec4e7b30614fc0c80a36c231d594086a0a7b59
Lulinha <luiz.viana000@gmail.com>
2015-10-30 19:26:30 -0300

    Add burrinho

---
+++ Assets/Scripts/Burrinho.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Burrinho : MonoBehaviour {
+
+	public float speed = 5f;
+
+	void Update () {
+
+		transform.Translate (Vector3.right * speed * Time.deltaTime);
+	}
+}
