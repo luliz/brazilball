19b0fd3ecf6d87867e02f869219bed15339bfac6
Lulinha <luiz.viana000@gmail.com>
2015-11-01 19:04:28 -0300

    Adicionada cena com o texto introdutório da fase 1

---
+++ Assets/eventSystem.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class eventSystem : MonoBehaviour {
+
+	void Update () {
+	
+		if (Input.GetKeyDown (KeyCode.Return)) {
+
+			Application.LoadLevel(0);
+		}
+	}
+}
