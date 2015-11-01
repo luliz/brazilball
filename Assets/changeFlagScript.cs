90cef5c38986e1aa8af9f99f205133c762db692e
Lulinha <luiz.viana000@gmail.com>
2015-11-01 01:04:52 -0300

    Adicionado mensagem avisando ao player para apertar o botão necessário na hora de entrar na casa ou mudar a bandeira

--- Assets/changeFlagScript.cs
+++ Assets/changeFlagScript.cs
@@ -1,15 +1,41 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class changeFlagScript : MonoBehaviour {
+
+	public GameObject message;
+	GameObject createdMessage;
+	bool interactable = true;
+
+	void OnTriggerEnter2D (Collider2D col) {
+		
+		if (col.CompareTag ("Player")) {
+			if (!createdMessage && interactable) {
+				createdMessage = Instantiate (message);
+			}
+		}
+	}
 
 	void OnTriggerStay2D (Collider2D col) {
 
 		if (col.CompareTag ("Player")) {
 
 			if (Input.GetKeyDown(Controls.enterHouse)) {
-
+				interactable = false;
+				if (createdMessage) {
+					Destroy (createdMessage);
+				}
 				this.GetComponent<Animator> ().SetTrigger ("change");
+			}
+		}
+	}
+
+	void OnTriggerExit2D (Collider2D col) {
+
+		if (col.CompareTag ("Player")) {
+			if (createdMessage) {
+				Destroy (createdMessage);
 			}
 		}
 	}
---
+++ Assets/updateText.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class updateText : MonoBehaviour {
+
+	Text text;
+	Transform player;
+	Vector3 playerScreenPosition;
+
+	void Awake () {
+		player = GameObject.Find ("Player").transform;
+		text = this.GetComponent<Text> ();
+	}
+	void Update () {
+		
+		playerScreenPosition = Camera.main.WorldToScreenPoint (player.position);
+		text.rectTransform.position = new Vector2 (playerScreenPosition.x, playerScreenPosition.y + 20f);
+	}
+}
