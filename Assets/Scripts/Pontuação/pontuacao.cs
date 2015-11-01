48ac86b3011651b0b36e1da26b7764344281bbfb
Refain <samuelcalebe@yahoo.com.br>
2015-11-01 11:54:07 -0200

    Sistema de Pontuação adicionado

---
+++ Assets/Scripts/Pontuação/pontuacao.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class pontuacao : MonoBehaviour {
+
+    public int moedas;
+    public int pontos;
+
+    public Text moedasT;
+    public Text pontosT;
+    
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        moedasT.text = moedas.ToString();
+        pontosT.text = pontos.ToString();
+
+    }
+}
--- Assets/Scripts/moedas.cs
+++ Assets/Scripts/moedas.cs
@@ -1,28 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class moedas : MonoBehaviour {
+public class moedas : MonoBehaviour
+{
 
     public Animator animator;
     public AudioClip soundshoot;
+
+    public GameObject pontuacao;
+
+ 
     // Use this for initialization
-    void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        animator.SetBool("movimento",true);
-	}
+ 
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetBool("movimento", true);
+    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
+
+            pontuacao.GetComponent<pontuacao>().moedas++;
+            pontuacao.GetComponent<pontuacao>().pontos+=10;
+
             AudioSource.PlayClipAtPoint(soundshoot, transform.position);
-            Destroy(this.gameObject);            
+            
+            Destroy(this.gameObject);
         }
 
-
     }
+
+
+
 }
