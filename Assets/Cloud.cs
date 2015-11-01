bea424cf4df15637f50da2b2a589b09ad9ae897f
Lulinha <luiz.viana000@gmail.com>
2015-11-01 01:38:11 -0300

    Adicionado sistema de spawnar as nuvens

---
+++ Assets/Cloud.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cloud : MonoBehaviour {
+
+	public Sprite[] nuvens;
+	SpriteRenderer spriteRenderer;
+	float speed;
+	void Awake () {
+
+		spriteRenderer = this.GetComponent<SpriteRenderer> ();
+	}
+	// Use this for initialization
+	void Start () {
+
+		int randomNumber = Random.Range (1, 9);
+
+		spriteRenderer.sprite = nuvens [randomNumber - 1];
+
+		speed = Random.Range (0.1f, 0.5f);
+	}
+	
+	void Update () {
+
+		transform.Translate (Vector3.left * speed * Time.deltaTime);
+	}
+}
---
+++ Assets/CloudMaker.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudMaker : MonoBehaviour {
+
+	public GameObject cloud;
+	float counter = 0;
+	Transform player;
+
+	void Awake () {
+
+		player = GameObject.Find ("Player").transform;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		transform.position = new Vector2 (player.position.x + 10f, 0);
+
+		if (counter <= 0) {
+			Vector3 positionToBeInstantiated = new Vector3 (transform.position.x, Random.Range (-0.35f, 1.5f));
+			Instantiate (cloud, positionToBeInstantiated, Quaternion.identity);
+			counter = Random.Range (1f, 2f);
+		}
+		counter -= Time.deltaTime;
+
+	}
+}
