fdbb77bea28a1b5173f977bb77ea4473400a3693
Atarico <luiz_paulo.09@hotmail.com>
2015-11-01 22:03:04 -0300

    Revert "Revert "Fiz tanta coisa que nao sei nem por onde começar""
    This reverts commit 13e7710490bbb857ac07182df639aa4a90c84f74.

---
+++ Assets/openPortao.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class openPortao : MonoBehaviour {
+
+
+    private int prosseguir;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        prosseguir = GameObject.Find("GameManager").GetComponent<GameManager>().index;
+
+        if (prosseguir >= 1)
+        {
+            if(GameObject.Find("Player").transform.position.x <= 212.4 && transform.position.y < -3.15)
+            {
+                transform.Translate(new Vector3(0, 0.5f * Time.deltaTime, 0));
+            }
+
+            if (GameObject.Find("Player").transform.position.x >= 212.4 && transform.position.y > -4.06)
+            {
+                transform.Translate(new Vector3(0, -0.5f * Time.deltaTime, 0));
+            }
+        }
+    }
+}
