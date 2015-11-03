0244006a66b705ef0c578cbb91fe4868a720541e
Lulinha <luiz.viana000@gmail.com>
2015-11-03 16:45:38 -0300

    Adicionado prefab do capacete e mudada a cor da letra

--- Assets/Scripts/Player/Controls.cs
+++ Assets/Scripts/Player/Controls.cs
@@ -8,8 +8,8 @@ public static class Controls : object
 	public static KeyCode walkLeft = KeyCode.A;
 	public static KeyCode walkRight = KeyCode.D;
 	public static KeyCode jump = KeyCode.Space;
-	public static KeyCode strongAttack = KeyCode.LeftControl;
-	public static KeyCode attack = KeyCode.LeftShift;
+	public static KeyCode strongAttack = KeyCode.K;
+	public static KeyCode attack = KeyCode.J;
 	public static KeyCode pause = KeyCode.Escape;
 	public static KeyCode climbLadderUp = KeyCode.W;
 	public static KeyCode climbLadderDown = KeyCode.S;
