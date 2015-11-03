98913971a0ea4d8ab665b17eeb6672097e54e0ea
Lulinha <luiz.viana000@gmail.com>
2015-11-03 14:48:55 -0300

    Adicionado mensagem dizendo q pode entrar na casa

---
+++ Assets/Capacete.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Capacete : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D col) {
+
+		if (col.tag == "Player") {
+
+			PlayerStatus.lives = 1;
+			Destroy (gameObject);
+		}
+	}
+}
