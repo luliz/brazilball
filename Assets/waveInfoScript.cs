a7fd1366c8fd4dff1bff013cadeafde7fe1d8be2
Lulinha <luiz.viana000@gmail.com>
2016-03-05 17:56:07 -0300

    Sistema de respawn da fase  feito

---
+++ Assets/waveInfoScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class waveInfoScript : MonoBehaviour {
+
+	float theFinalCountdown = 2;
+	
+	// Update is called once per frame
+	void Update () {
+
+		theFinalCountdown -= Time.deltaTime;
+		if (theFinalCountdown <= 0f) {
+			Destroy (this.gameObject);
+		}
+	}
+}
