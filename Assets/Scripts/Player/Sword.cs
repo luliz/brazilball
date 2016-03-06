4d1e88a066933d0bb50fe49b474439b552c2c73e
Refain <samuelcalebe@yahoo.com.br>
2016-03-06 11:27:13 -0300

    Resolvendo um conflito de AI´s da fase 1 e 2
    agora vai

--- Assets/Scripts/Player/Sword.cs
+++ Assets/Scripts/Player/Sword.cs
@@ -1,26 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Sword : MonoBehaviour {
-    
+public class Sword : MonoBehaviour
+{
 
 
-	void OnTriggerEnter2D (Collider2D col) {
 
-		if (col.gameObject.CompareTag ("enemy")) {
+    void OnTriggerEnter2D(Collider2D col)
+    {
 
-			if (col.gameObject.GetComponent<AIzarabatana>()) {
-            	col.gameObject.GetComponent<AIzarabatana>().Take();
-			}
-			if (col.gameObject.GetComponent<BasicAI> ()) {
-				col.gameObject.GetComponent<BasicAI> ().TakeDamage();
-			}
-            
+        if (col.gameObject.CompareTag("enemy"))
+        {
+            col.gameObject.GetComponent<BasicAI>().TakeDamage();
 
-		}
-		if (col.gameObject.CompareTag ("enemyArcher")) {
+        }
+        if (col.gameObject.CompareTag("enemyArcher"))
+        {
 
-			col.gameObject.GetComponent<Archer2> ().TakeDamage();
-		}
-	}
+            col.gameObject.GetComponent<Archer2>().TakeDamage();
+        }
+
+        if (col.gameObject.CompareTag("enemy2"))
+        {
+
+            col.gameObject.GetComponent<AIzarabatana>().Take();
+
+        }
+
+
+
+    }
 }
+	
+
