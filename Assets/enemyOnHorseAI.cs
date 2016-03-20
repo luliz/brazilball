95008f8bafaab466d357221892cf673037e72e18
Lulinha <luiz.viana000@gmail.com>
2016-03-20 20:01:29 -0300

    Iniciado a AI do inimigo no cavalo

---
+++ Assets/enemyOnHorseAI.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyOnHorseAI : MonoBehaviour {
+
+	public Transform target;
+	public Transform enemy;
+	public GameObject bala;
+	public Transform bicoDaArma;
+
+	public float tempoEntreTiros;
+	public float[] possiveisPosicoes;
+	public float[] tamanhosPorPosicao;
+
+	public float velocidadeMin;
+	public float velocidadeMax;
+	public float posicaoInicial;
+
+	private float ContagemRegressivaParaAtirar;
+	private float ladoQueNasce;
+	private float posicaoVerticalQueNasce;
+	private float velocidade;
+
+	// Use this for initialization
+	void Start () {
+
+		ContagemRegressivaParaAtirar = tempoEntreTiros;
+
+		ladoQueNasce = Random.value;
+		if (ladoQueNasce > 0.5f)
+			ladoQueNasce = 1f;
+		else
+			ladoQueNasce = -1f;
+		
+
+		posicaoVerticalQueNasce = Random.value;
+		if (posicaoVerticalQueNasce > 0.66f)
+			posicaoVerticalQueNasce = 0f;
+		else if (posicaoVerticalQueNasce > 0.33f)
+			posicaoVerticalQueNasce = 1f;
+		else
+			posicaoVerticalQueNasce = 2f;
+
+		velocidade = Random.Range (velocidadeMin, velocidadeMax);
+		enemy.localScale = new Vector3(-ladoQueNasce*enemy.localScale.x, enemy.localScale.y);
+		transform.localScale = new Vector2 (tamanhosPorPosicao [(int)posicaoVerticalQueNasce], tamanhosPorPosicao [(int)posicaoVerticalQueNasce]);
+
+		transform.position = new Vector2 (posicaoInicial * ladoQueNasce, possiveisPosicoes [(int) posicaoVerticalQueNasce]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		ContagemRegressivaParaAtirar -= Time.deltaTime;
+
+		transform.Translate (Vector2.right * velocidade * -ladoQueNasce);
+		if ((transform.position.y == target.position.y) && ContagemRegressivaParaAtirar <= 0) {
+
+			GameObject balaInstanciada = (GameObject) Instantiate (bala, bicoDaArma.position, Quaternion.identity);
+			balaInstanciada.GetComponent<bullet> ().moveSpeed *= (int) -ladoQueNasce;
+			ContagemRegressivaParaAtirar = tempoEntreTiros;
+		}
+	}
+}
