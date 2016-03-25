ae774bed42cba1e90d89479eb008c6901e10e187
Lulinha <luiz.viana000@gmail.com>
2016-03-25 18:35:08 -0300

    feito o sistema do fundo da fase 05

---
+++ Assets/Background.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Background : MonoBehaviour {
+
+	public float velocidade;
+	
+	// Update is called once per frame
+	void Update () {
+	
+		transform.Translate (Vector2.left * velocidade * Time.deltaTime);
+		if (transform.position.x <= -30) {
+
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/backgroundManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class backgroundManager : MonoBehaviour {
+
+	public GameObject gameObjectChao;
+	public Sprite[] backgroundSprites;
+
+	private Transform chaoParent;
+
+	// Use this for initialization
+	void Start () {
+	
+		chaoParent = GameObject.Find ("chaoParent").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (chaoParent.childCount <= 2) {
+
+			CreateNewBackground ();
+		}
+	}
+
+	void CreateNewBackground () {
+
+		int numChildren = chaoParent.childCount;
+		Vector2 positionToInstantiate = new Vector2 (chaoParent.GetChild (numChildren - 1).position.x + 22.07f, 0);
+		GameObject chaoInstanciado = (GameObject) Instantiate (gameObjectChao, positionToInstantiate, Quaternion.identity);
+		chaoInstanciado.transform.SetParent (chaoParent, true);
+		chaoInstanciado.GetComponent<SpriteRenderer> ().sprite = backgroundSprites [Random.Range (0, backgroundSprites.Length - 1)];
+	}
+}
